22eba56443ba5a425c0200c555b43b97894778a8
Mitrofanov Vitaliy <VMitrofanov@korusconsulting.ru>
2025-05-25 15:10:17 +0300

    Удален README.md, так как будет использоваться другой

---
+++ target/compiled/test_project/models/example/schema.yml/not_null_my_first_dbt_model_id.sql
@@ -0,0 +1,11 @@
+
+    
+    
+
+
+
+select id
+from "dbt"."dbt"."my_first_dbt_model"
+where id is null
+
+
---
+++ target/compiled/test_project/models/example/schema.yml/not_null_my_second_dbt_model_id.sql
@@ -0,0 +1,11 @@
+
+    
+    
+
+
+
+select id
+from "dbt"."dbt"."my_second_dbt_model"
+where id is null
+
+
